35b2de4277c1d823e027b6810fdbd3361de8630f
hrl-2024 <hrl@bu.edu>
2023-11-29 12:18:51 -0500

    Networking Basics with Retrofit: Defining an API interface

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,5 +4,7 @@ import retrofit2.http.GET
 
 interface FlickrApi {
     @GET("/")
-    suspend fun fetchContents(): String
+    suspend fun fetchContents(): String  // A general response type called OkHttp.ResponseBody
+                                            // is provided with Retrofit.
+                                            // But here, we want it parse into a String object
 }
