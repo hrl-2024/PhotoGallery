2efc8e74f8ca538139283cbe253085a4ef05414b
hrl-2024 <hrl@bu.edu>
2023-12-04 16:02:47 -0500

    (comment out generateAdapter solves issue) Fetching JSON from Flickr: Deserializing JSON text into model objects

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrResponse.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrResponse.kt
@@ -2,7 +2,7 @@ package com.bignerdranch.android.photogallery.api
 
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+//@JsonClass(generateAdapter = true)
 data class FlickrResponse(
     val photos: PhotoResponse
 )
--- app/src/main/java/com/bignerdranch/android/photogallery/api/GalleryItem.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/GalleryItem.kt
@@ -3,9 +3,9 @@ package com.bignerdranch.android.photogallery.api
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+//@JsonClass(generateAdapter = true)
 data class GalleryItem(
     val title: String,
     val id: String,
-    @Json(name = "url_s") val url: String,
+    @Json(name = "url_s") val url: String
 )
