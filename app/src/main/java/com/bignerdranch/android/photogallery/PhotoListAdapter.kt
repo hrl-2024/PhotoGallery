12df52569ec2510872e847953c4895af14db7640
hrl-2024 <hrl@bu.edu>
2023-12-12 16:33:42 -0500

    Displaying Results in RecyclerView: Displaying images (use coil load)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoListAdapter.kt
@@ -3,12 +3,15 @@ package com.bignerdranch.android.photogallery
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.bignerdranch.android.photogallery.api.GalleryItem
 import com.bignerdranch.android.photogallery.databinding.ListItemGalleryBinding
 
 class PhotoViewHolder(private val binding: ListItemGalleryBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(galleryItem: GalleryItem) {
-      // TODO
+        binding.itemImageView.load(galleryItem.url) {
+            placeholder(R.drawable.bill_up_close)
+        }
     }
 }
 
